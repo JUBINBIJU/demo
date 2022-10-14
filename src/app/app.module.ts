7c20d277f0b97750bd873b056515a92e4348ea67
Jubin Biju Koruthu <jubin.biju@gadgeon.inc>
2022-10-14 12:55:56 +0530

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule,Routes} from '@angular/router'
+
+const appRoute:Routes=
+[
+    {path:'',component:HomeComponent},
+    {path:'home',component:HomeComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'about',component:AboutComponent},
+    {path:'services',component:ServicesComponent}
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoute)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
